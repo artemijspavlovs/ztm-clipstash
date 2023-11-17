a8f4dfc00d190621347782d49ecf74e0d506af69
artemijspavlovs <artemijs.pavlovs@artpav.dev>
2023-11-17 09:46:42 +0200

    feat: add clip struct and fields

---
+++ src/lib/domain/clip/field/mod.rs
@@ -0,0 +1,25 @@
+// pub use exports the structures into the parent module
+// remove the need to access the files directly from outside the module
+pub clip_id;
+pub use clip_id::ClipId;
+
+pub shortcode;
+pub use shortcode::ShortCode;
+
+pub content;
+pub use content::Content;
+
+pub title;
+pub use title::Title;
+
+pub posted;
+pub use posted::Posted;
+
+pub expires;
+pub use expires::Expires;
+
+pub password;
+pub use password::Password;
+
+pub hits;
+pub use field::Hits;
