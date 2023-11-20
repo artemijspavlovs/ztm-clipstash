f21a8b7085e33d784a8ba666fed6c27f3e93c0ef
artemijspavlovs <artemijs.pavlovs@artpav.dev>
2023-11-20 14:00:09 +0200

    feat: add functions to retrieve and create new Clips in the database

---
+++ src/lib/data/query.rs
@@ -0,0 +1,77 @@
+use super::model;
+use crate::data::{DataError, DatabasePool};
+
+// type alias on Result makes it easier to leverage a Result with DataError
+// wihout having to type it out every time
+type Result<T> = std::result::Result<T, DataError>;
+
+// `get_clip` function accepts a generic type M which should be a model::GetClip
+// Into tries to transform any data that is passed into the function into a model::GetClip
+// and returns a compiler error if it fails to do so
+pub async fn get_clip<M: Into<model::GetClip>>(
+    model: M,
+    pool: &DatabasePool,
+) -> Result<model::Clip> {
+    let model = model.into();
+    let shortcode = model.shortcode.as_str();
+    Ok(sqlx::query_as!(
+        model::Clip,
+        "SELECT * FROM clips WHERE shortcode = ?",
+        shortcode,
+    )
+    .fetch_one(pool)
+    .await?)
+}
+
+pub async fn new_clip<M: Into<model::NewClip>>(
+    model: M,
+    pool: &DatabasePool,
+) -> Result<model::Clip> {
+    let model = model.into();
+    let _ = sqlx::query!(
+        r#"INSERT INTO clips (
+            clip_id,
+            shortcode,
+            content,
+            title,
+            posted,
+            expires,
+            password,
+            hits)
+           VALUES (?, ?, ?, ?, ?, ?, ?, ?)"#,
+        model.clip_id,
+        model.shortcode,
+        model.content,
+        model.title,
+        model.posted,
+        model.expires,
+        model.password,
+        0
+    )
+    .execute(pool)
+    .await?;
+    get_clip(model.shortcode, pool).await
+}
+
+pub async fn update_clip<M: Into<model::UpdateClip>>(
+    model: M,
+    pool: &DatabasePool,
+) -> Result<model::Clip> {
+    let model = model.into();
+    let _ = sqlx::query!(
+        r#"UPDATE clips SET
+            content = ?,
+            expires = ?,
+            password = ?,
+            title = ?
+           WHERE shortcode = ?"#,
+        model.content,
+        model.expires,
+        model.password,
+        model.title,
+        model.shortcode
+    )
+    .execute(pool)
+    .await?;
+    get_clip(model.shortcode, pool).await
+}
